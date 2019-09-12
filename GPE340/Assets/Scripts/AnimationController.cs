ba98c9f859a0e77c53cf7b913e87abe152e5e59b
ETHAN DEAN <ETHDEAN@uat.edu>
2019-09-12 15:42:41 -0700

    What I have so far

---
+++ GPE340/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(anim == null)
+        {
+            anim = GetComponent<Animator>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+    }
+}
